9d9b7c70ac920db623a1d2054e59c872bcc52ba4
blacknii <42908977+blacknii@users.noreply.github.com>
2023-06-14 19:06:53 +0200

    .

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,5 +1,5 @@
-import React, { useMemo } from "react";
-import { useTable } from "react-table";
+import React, { useMemo, useState } from "react";
+import { useTable, useRowSelect } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./columns";
 import "./table.css";
@@ -8,10 +8,7 @@ export const BasicTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
 
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
+  const [selectedRowId, setSelectedRowId] = useState(null);
 
   const {
     getTableProps,
@@ -20,41 +17,64 @@ export const BasicTable = () => {
     footerGroups,
     rows,
     prepareRow,
-  } = tableInstance;
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useRowSelect
+  );
+
+  const handleRowClick = (row) => {
+    setSelectedRowId(row.id);
+    console.log(row.id);
+  };
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map((footerGroup) => (
-          <tr {...footerGroup.getFooterGroupProps()}>
-            {footerGroup.headers.map((column) => (
-              <td {...column.getFooterGroupProps}>{column.render("Footer")}</td>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps({
+                  onClick: () => handleRowClick(row),
+                  style: {
+                    backgroundColor:
+                      row.id === selectedRowId ? "lightgray" : "white",
+                  },
+                })}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
   );
 };
 
