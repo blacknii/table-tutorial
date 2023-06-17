d0204b59a4a98c5d66bb97d68613bdcd53a430bd
blacknii <42908977+blacknii@users.noreply.github.com>
2023-06-17 21:48:33 +0200

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import "../style/home.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
+  const [search, setSearch] = useState("");
+  const [authors, setAuthors] = useState([]);
+
+  const searchBook = () => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q=inauthor:" +
+          search +
+          "&key=AIzaSyD9AulRjx3A6ZJb7cP4e_t1r8Ow0pyfuXw"
+      )
+      .then((res) => {
+        let authorsSet = new Set();
+        for (let item of res.data.items) {
+          if (item.volumeInfo.authors) {
+            for (let author of item.volumeInfo.authors) {
+              authorsSet.add(author);
+            }
+          }
+        }
+        setAuthors([...authorsSet]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleKeyPress = (evt) => {
+    if (evt.key === "Enter") {
+      searchBook();
+    }
+  };
+
   return (
     <div className="home">
       <h1>HOME</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter Your Author Name"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={searchBook}>search</button>
+      </div>
+      <br />
       <Link to={"table/J. R. R. Tolkien/1"}>go to table</Link>
+      {authors.map((author) => (
+        <Link to={`table/${author}/1`}>
+          <h3 className="title">{author}</h3>
+        </Link>
+      ))}
     </div>
   );
 }
